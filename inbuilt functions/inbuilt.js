42f76e30fa6e0b1a16259467bc9fd37bd86878dc
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-08-06 13:22:20 +0530

    inbuilt function

---
+++ inbuilt functions/inbuilt.js
@@ -0,0 +1,16 @@
+
+setTimeout(function () {
+    console.log('GA')
+},4000)
+
+
+
+
+//  setInterval(function () {
+//     document.getElementById("abc").innerHTML=new Date().toLocaleTimeString();
+// })
+
+// setInterval(function () {
+//         console.log('GA')
+//      },1000)
+    
