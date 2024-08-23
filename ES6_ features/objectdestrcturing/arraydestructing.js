fb97243d71f832682ad4ee6ccefc724d90a6572e
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-08-23 13:59:22 +0530

    objectdestructing

---
+++ ES6_ features/objectdestrcturing/arraydestructing.js
@@ -0,0 +1,7 @@
+let a=10;
+let b=20;
+/*
+swapping 2 numbers without 3 rd variable
+*/
+[b,a]=[a,b]; //array destructing
+console.log(a,b)
---
+++ ES6_ features/objectdestrcturing/two.js
@@ -0,0 +1,16 @@
+let state={
+    emp:{
+         id:101,
+         name:'priyanka',
+         sal: 45000
+        },
+    product:{
+        p_name:'samsung pod',
+        price:25000
+    }
+
+}
+let {emp}=state;
+let {name}=emp;
+console.log(emp);
+console.log(name);
