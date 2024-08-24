c578be4c1a7a507a711bab213abfba24c21389b1
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-08-24 13:29:39 +0530

    promises

---
+++ ES6_ features/promises/data3.js
@@ -0,0 +1,26 @@
+/*
+   promise:passing a function with 2 args resolve and reject
+
+           once promise is received,executes
+           .then(success msg)
+           .catch(failure err)
+*/
+
+
+
+
+let employees=[
+    {id:101,name:"Rahul",sal:45000},
+    {id:102,name:"Sonia",sal:55000}
+]
+let createEmployee = ()=>{
+return new Promise((resolve,reject)=>{
+   setTimeout(()=>{
+       let db_flag = true;
+       db_flag===true? resolve('Data Inserted'):reject("Failed")
+   },4000)
+})
+}
+let displayEmployee=()=>{
+}
+createEmployee({id:103,name:"Priyanka",sal:65000})
