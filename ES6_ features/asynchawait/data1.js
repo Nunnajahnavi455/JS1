71ad3864054cc2cc682a324760b8719c4bcaa1fb
jahnavi Nunna <jahnavinunna6@gmail.com>
2024-08-24 13:44:34 +0530

    promises

---
+++ ES6_ features/asynchawait/data1.js
@@ -0,0 +1,15 @@
+let employees=[
+    {id:101,name:"Rahul",sal:45000},
+    {id:102,name:"Sonia",sal:55000}
+]
+let createEmployee=(emp)=>{
+
+}
+let displayEmployee=()=>{
+
+}
+let lunchTime =()=>{
+    createEmployee({id:103,name:"priya",sal:65000})
+    getEmployees()
+}
+lunchTime()
